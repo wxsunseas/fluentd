65a42903963f14bfa6286c95237ccb74b71e9280
lanlandehai100 <lanlandehai100@sina.com>
2022-12-13 06:51:03 +0800

    使用变量名

--- fluentbit/test.sh
+++ fluentbit/test.sh
@@ -1,3 +1,12 @@
+docker run -itd  --name factual \
+    --network fluent \
+    -v /Users/lijinhai/Documents/project/fluentd/fluentbit/factual:/usr/local/fluent/fluentbit  \
+    -v /Users/lijinhai/Documents/project/fluentd/logs:/factal/logs  \
+    -v /usr/share/zoneinfo/Asia/Shanghai:/etc/localtime \
+    cr.fluentbit.io/fluent/fluent-bit:2.0.5 \
+    -c /usr/local/fluent/fluentbit/fluent-bit.conf 
+
+
 docker run -itd  --name quantf \
     --network fluent \
     -v /Users/lijinhai/Documents/project/fluentd/fluentbit/quant:/usr/local/fluent/fluentbit  \
