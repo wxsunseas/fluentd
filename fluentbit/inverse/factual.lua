0517e031d28a97a5efdf01771148620a26717cc2
lanlandehai100 <lanlandehai100@sina.com>
2023-01-03 22:54:00 +0800

    inverse

---
+++ fluentbit/inverse/factual.lua
@@ -0,0 +1,25 @@
+
+function getLogType(tag, timestamp, record) 
+    newRecord = record
+    if string.match(record["log"], "ERROR") ~= nil then
+        newRecord["lo11"] = string.match(record["log"], "ERROR")
+    elseif string.match(record["log"], "WARN") ~= nil then
+        newRecord["lo11"] = string.match(record["log"], "WARN")
+    elseif string.match(record["log"], "INFO") ~= nil then
+        newRecord["lo11"] = string.match(record["log"], "INFO")
+    elseif string.match(record["log"], "DEBUG") ~= nil then
+        newRecord["lo11"] = string.match(record["log"], "DEBUG")
+    end
+    return 1, timestamp, newRecord
+end
+
+function getUpdateTime(tag, timestamp, record) 
+    newRecord = record
+    if string.match(record["log"], "([0-9-]+ [:0-9.]+)") ~= nil then
+        newRecord["lo07"] = string.match(record["log"], "([0-9-]+ [:0-9.]+)")
+        return 1, timestamp, newRecord
+    else
+        newRecord["lo07"] = 'None'
+        return 1, timestamp, newRecord
+    end
+end
